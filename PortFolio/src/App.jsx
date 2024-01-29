56bc7e1c93481691e5c7c7206180e294daca8c95
rp-axelor <r.purabia@axelor.com>
2024-01-29 18:12:53 +0530

    Update: NavBar updated

--- PortFolio/src/App.jsx
+++ PortFolio/src/App.jsx
@@ -1,12 +1,14 @@
 import Body from './components/Body'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import NavBar from './components/NavBar'
 import './index.css'
 
 function App() {
 
   return (
     <>
+    <NavBar/>
       <Header/>
       <Body />
       <Footer />
