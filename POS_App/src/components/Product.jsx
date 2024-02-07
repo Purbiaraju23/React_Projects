0049c90218b2593d0ba605e0b8a6ed2c46e1a3d9
rp-axelor <r.purabia@axelor.com>
2024-02-07 13:25:52 +0530

    Implemented three tasks in the POS app: 1. Retrieving data from JSON file and mapping the data. 2. Implementing product filtering based on type. 3. Adding functionality to sort items based on name, price (low to high and high to low).

--- POS_App/src/components/Product.jsx
+++ POS_App/src/components/Product.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-function Product({product, onAddToCart}) {
+function Product({ product, onAddToCart }) {
   return (
-
     <div className="card">
-        <div className="card-body">
-            <div className="row">
-                <div className="col-md-12">
-                    
-                <div style={{backgroundImage:`url(${product.imageUrl})`, backgroundSize: 'contain',
-                            backgroundRepeat: 'no-repeat', height: '150px'}}>
-
-                </div>
-                <div className="col-md-12">
-                    <div className="card-title h5">
-                        {product.name}
-                    </div>
-                    <p className='card-text'>
-                        <b>
-                            ₹ {product.price}
-                        </b>
-                    </p>
-                    <button type='button' className='btn btn-primary' onClick={() => onAddToCart(product)}>Add To Cart</button>
-                </div>
-                </div>
+      <div className="card-body">
+        <div className="row">
+          <div className="col-md-12">
+            <div
+              style={{
+                backgroundImage: `url(${product.imageUrl})`,
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+                height: "150px",
+              }}
+            ></div>
+            <div className="col-md-12">
+              <div className="card-title h5">{product.name}</div>
+              <p className="card-text">
+                <b>₹ {product.price}</b>
+              </p>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => onAddToCart(product)}
+              >
+                Add To Cart
+              </button>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
-
-
+export default Product;
